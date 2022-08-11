b9852d9721411ef1da46cc3e6d1ac0f93c860a12
Sergio <winksousa0@gmail.com>
2022-08-11 19:41:24 -0300

    responsivo

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,7 @@ import { Profile } from "./components/Profile";
 function App() {
   return (
     <div className="">
-      <img
-        src="../../public/githublogo.png"
-        alt=""
-        className="m-auto mt-24  w-12"
-      />
+      <img src="./github.png" alt="" className="m-auto mt-24  w-12" />
       <Profile />
     </div>
   );
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -22,38 +22,41 @@ export function Profile() {
   }
 
   return (
-    <div className="bg-neutral-50 w-[700px] p-6  m-auto mt-12 rounded-xl">
-      <div className="flex justify-center">
-        <input
-          type="text"
-          value={name}
-          className="border-2 rounded-md mr-2 outline-none p-2"
-          onChange={(e) => setName(e.target.value)}
-        />
-        <button onClick={handleApi}>
-          <MagnifyingGlass size={28} />
-        </button>
+    <>
+      <img src="../github.png" alt="" className="m-auto mt-24  w-12" />
+      <div className="bg-neutral-50 w-3/4 md:w-[700px] p-6  m-auto mt-8 md:mt-12 mb-4 rounded-xl">
+        <div className="flex justify-center">
+          <input
+            type="text"
+            value={name}
+            className="border-2 md:w-64 rounded-md mr-2 outline-none p-2 w-full"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button onClick={handleApi}>
+            <MagnifyingGlass size={28} />
+          </button>
+        </div>
+        <div className="flex flex-col items-center gap-2 mt-8">
+          <img src={profile?.avatar_url} alt="" className="w-28 rounded-full" />
+          <a href={profile?.html_url} className="text-2xl" target="_black">
+            {profile?.name}
+          </a>
+          <span className="text-zinc-500">{profile?.bio}</span>
+          {profile?.name ? (
+            <div className="flex md:flex-row flex-col gap-10 mt-12">
+              <span className="text-xl">
+                {profile?.public_repos} Repositories
+              </span>
+              <span className="border-b-2 md:border-r-2 border-black"></span>
+              <span className="text-xl">{profile?.followers} Followers</span>
+              <span className="border-b-2 md:border-r-2 border-black"></span>
+              <span className="text-xl">{profile?.following} Following</span>
+            </div>
+          ) : (
+            ""
+          )}
+        </div>
       </div>
-      <div className="flex flex-col items-center gap-2 mt-8">
-        <img src={profile?.avatar_url} alt="" className="w-28 rounded-full" />
-        <a href={profile?.html_url} className="text-2xl" target="_black">
-          {profile?.name}
-        </a>
-        <span className="text-zinc-500">{profile?.bio}</span>
-        {profile?.name ? (
-          <div className="flex gap-10 mt-12">
-            <span className="text-xl">
-              {profile?.public_repos} Repositories
-            </span>
-            <span className="border-r-2 border-black"></span>
-            <span className="text-xl">{profile?.followers} Followers</span>
-            <span className="border-r-2 border-black"></span>
-            <span className="text-xl">{profile?.following} Following</span>
-          </div>
-        ) : (
-          ""
-        )}
-      </div>
-    </div>
+    </>
   );
 }
