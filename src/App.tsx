683456db9e6d5c99f9cfb053ddee29fbcd88fd6f
Sergio <winksousa0@gmail.com>
2022-08-11 19:21:38 -0300

    img

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,11 @@ import { Profile } from "./components/Profile";
 function App() {
   return (
     <div className="">
-      <img src="../../public/25231.png" alt="" className="m-auto mt-24  w-12" />
+      <img
+        src="../../public/githublogo.png"
+        alt=""
+        className="m-auto mt-24  w-12"
+      />
       <Profile />
     </div>
   );
