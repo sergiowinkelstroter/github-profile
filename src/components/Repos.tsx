c4e6fce7211e74d23601833645b67b934ef58b2f
Sergio <winksousa0@gmail.com>
2022-08-15 11:02:46 -0300

    position button voltar

--- src/components/Repos.tsx
+++ src/components/Repos.tsx
@@ -7,13 +7,11 @@ export const Repos = () => {
 
   return (
     <div className="bg-neutral-50 w-3/4 md:w-[700px] p-6  m-auto mt-8 md:mt-12 mb-4 rounded-xl">
-      <div className="block md:flex items-center justify-center  ">
-        <h1 className="text-xl text-center relative">
-          Repositories {userData?.name}
-        </h1>
+      <div className="block md:flex items-center justify-center relative">
+        <h1 className="text-xl text-center ">Repositories {userData?.name}</h1>
         <Link
           to="/"
-          className="absolute  hover:opacity-50 left-36 md:left-[900px]"
+          className="absolute  hover:opacity-50 left-44 md:left-[600px] "
         >
           Voltar
         </Link>
